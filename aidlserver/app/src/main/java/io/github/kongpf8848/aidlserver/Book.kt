03d23f52c6cbeba98402cd608d8f25012d2e3f2e
Jack kong <kongpf8848@gmail.com>
2023-08-03 10:47:51 +0800

    update code

--- aidlserver/app/src/main/java/io/github/kongpf8848/aidlserver/Book.kt
+++ aidlserver/app/src/main/java/io/github/kongpf8848/aidlserver/Book.kt
@@ -1,69 +1,7 @@
-package io.github.kongpf8848.aidlserver;
+package io.github.kongpf8848.aidlserver
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-public class Book implements Parcelable {
-    private int bookId;
-    private String bookName;
-
-    public int getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-
-    public Book(int bookId, String bookName) {
-        this.bookId = bookId;
-        this.bookName = bookName;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "bookId=" + bookId +
-                ", bookName='" + bookName + '\'' +
-                '}';
-    }
-
-    protected Book(Parcel in) {
-        bookId = in.readInt();
-        bookName = in.readString();
-    }
-
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(bookId);
-        dest.writeString(bookName);
-    }
-
-    public static final Creator<Book> CREATOR = new Creator<Book>() {
-        @Override
-        public Book createFromParcel(Parcel in) {
-            return new Book(in);
-        }
-
-        @Override
-        public Book[] newArray(int size) {
-            return new Book[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-}
+@Parcelize
+class Book(val bookId: Int,val bookName: String) : Parcelable
